af6f962ece8256ec3fe6f6c6327925471bcca55d
Verity Stothard <vjstothard@gmail.com>
2018-07-29 16:01:11 +1000

    WIP setting up CMS

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <nuxt/>
+    <script>
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        });
+      }
+    </script>
   </div>
 </template>
 
